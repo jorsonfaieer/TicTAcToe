d354848ee942c95a540e7cb7104ff67a4822f69a
Gaston Vazquez <gaston.e.vazquez@gmail.com>
2023-02-28 08:42:09 -0300

    se agrega funcion para verificar ganador

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,46 @@
 function resetearJuego(){
     casillas.forEach(element => {
         element.innerHTML = '';
+        turno = "X";
+        ganador = false;
+        mensaje.innerHTML = 'Es el turno del jugador ' + turno + '.';
     });
 }
 
+function chequearGanador(player){
+    
+    // verificar filas
+    if(casillas[0].innerHTML === player && casillas[1].innerHTML === player && casillas[2].innerHTML === player){
+        return true;
+    }
+    if(casillas[3].innerHTML === player && casillas[4].innerHTML === player && casillas[5].innerHTML === player){
+        return true;
+    }
+    if(casillas[6].innerHTML === player && casillas[7].innerHTML === player && casillas[8].innerHTML === player){
+        return true;
+    }
+
+    // verificar columnas
+    if(casillas[0].innerHTML === player && casillas[3].innerHTML === player && casillas[6].innerHTML === player){
+        return true;
+    }
+    if(casillas[1].innerHTML === player && casillas[4].innerHTML === player && casillas[7].innerHTML === player){
+        return true;
+    }
+    if(casillas[2].innerHTML === player && casillas[5].innerHTML === player && casillas[8].innerHTML === player){
+        return true;
+    }
+
+    // verificar diagonales
+    if(casillas[0].innerHTML === player && casillas[4].innerHTML === player && casillas[8].innerHTML === player){
+        return true;
+    }
+    if(casillas[2].innerHTML === player && casillas[4].innerHTML === player && casillas[6].innerHTML === player){
+        return true;
+    }
+
+    return false;
+}
 
 const casillas = document.querySelectorAll('#casilla');
 const mensaje = document.querySelector('#mensaje');
@@ -12,19 +49,26 @@ let turno = "X";
 let ganador = false;
 
 
-casillas.forEach(element => {
-    element.addEventListener("click", ()=>{
-        if (!ganador && element.innerHTML === '') {
-            element.innerHTML = turno;
+casillas.forEach(casillero => {
+    casillero.addEventListener("click", ()=>{
+        if (!ganador && casillero.innerHTML === '') {
+            casillero.innerHTML = turno;
 
             // Verificar si el jugador actual gano
-
+            if(chequearGanador(turno)){
+                ganador = true;          
+            }
             // Verificar si hay empate
 
             // Cambio de turno
-            turno = turno === 'X' ? 'O' : 'X';
-            mensaje.innerHTML = 'Es el turno del jugador ' + turno + '.';
-        }        
+            if(!ganador){
+                turno = turno === 'X' ? 'O' : 'X';
+                mensaje.innerHTML = `Es el turno del jugador ${turno}.`;
+            }
+        }
+        if (ganador){
+            mensaje.innerHTML = `El jugador ${turno} ha ganado!`; 
+        }
     });    
 });    
 
